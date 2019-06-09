8ce5f78c0da51434e1b109392a20db0910855a58
Limitless99 <51519171+Limitless99@users.noreply.github.com>
2019-06-09 16:29:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,9 +30,9 @@ variable "environment" { default = "dev" }
 variable "instance_type" {
   type = "map"
   default = {
-    dev = "t2.nano"
+    dev = "t2.micro"
     test = "t2.micro"
-    prod = "t2.medium"
+    prod = "t2.micro"
     }
 }
 
